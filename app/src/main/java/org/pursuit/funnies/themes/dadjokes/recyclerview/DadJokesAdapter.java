581c1555abfd47718c39c16990f639f6f6d2cbd3
Luis Jimenez <luisjimenez@pursuit.org>
2019-04-12 04:04:12 -0400

    Started DadJokes RecyclerView from scratch

--- app/src/main/java/org/pursuit/funnies/themes/dadjokes/recyclerview/DadJokesAdapter.java
+++ app/src/main/java/org/pursuit/funnies/themes/dadjokes/recyclerview/DadJokesAdapter.java
@@ -1,8 +1,6 @@
-package org.pursuit.funnies.themes.dadjokes;
+package org.pursuit.funnies.themes.dadjokes.recyclerview;
 
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,32 +8,26 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.pursuit.funnies.R;
+import org.pursuit.funnies.themes.dadjokes.models.Joke;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class DadJokesAdapter extends RecyclerView.Adapter<DadJokesViewHolder> {
-    List<String> jokeList;
-    TextView textView;
 
 
     @NonNull
     @Override
     public DadJokesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View childView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_view,viewGroup,false);
-        return new DadJokesViewHolder(childView);
+        return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull DadJokesViewHolder dadJokesViewHolder, int i) {
-    dadJokesViewHolder.onBind(jokeList.get(i));
-        CardView cardView = dadJokesViewHolder.cardView;
 
     }
 
-
     @Override
     public int getItemCount() {
-        return jokeList.size();
+        return 0;
     }
 }
